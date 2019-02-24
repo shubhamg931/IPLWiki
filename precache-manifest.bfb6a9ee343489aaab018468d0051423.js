1e818e996067ad97e3f210b4c4a6a8adad1cd09c
shubhamg931 <sg270798@gmail.com>
2019-02-25 01:20:51 +0530

    Updates

---
+++ precache-manifest.bfb6a9ee343489aaab018468d0051423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3e6fb45ed58b7047a9c",
+    "url": "/socialcops-assignment/static/css/main.34d88eab.chunk.css"
+  },
+  {
+    "revision": "c3e6fb45ed58b7047a9c",
+    "url": "/socialcops-assignment/static/js/main.c3e6fb45.chunk.js"
+  },
+  {
+    "revision": "b03f71b2f0e622dbf1f3",
+    "url": "/socialcops-assignment/static/css/1.a6701498.chunk.css"
+  },
+  {
+    "revision": "b03f71b2f0e622dbf1f3",
+    "url": "/socialcops-assignment/static/js/1.b03f71b2.chunk.js"
+  },
+  {
+    "revision": "028270ae305f784c0933",
+    "url": "/socialcops-assignment/static/js/runtime~main.028270ae.js"
+  },
+  {
+    "revision": "4ba55a51ba5129b9eb592fb027a724d3",
+    "url": "/socialcops-assignment/index.html"
+  }
+];
